b435f9727c8e3f3b87872a9ea60845cccae0a305
suresh170411 <suresh.20fk@gmail.com>
2023-06-22 11:26:06 +0530

    Registration form added for front end

---
+++ FrontEntIntegration/registration.js
@@ -0,0 +1,31 @@
+function registerStudent(){
+
+    let sname=document.getElementById("name").value;
+    let sroll=document.getElementById("roll").value;
+    let sage = document.getElementById("age").value;
+    let smarks=document.getElementById("marks").value;
+    
+    fetch("http://localhost:8888/student", {
+
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            "roll":sroll,
+            "name": sname,
+            "age": sage,
+            "marks": smarks
+        })
+    }).then(response => {
+        if(response.status == 201 | response.status == 200){
+            response.json().then(data => {
+                alert("Student sucessfully registered with Roll: "+data.roll)
+            });
+        }else{
+            response.json().then(data => alert(data.message));
+        }
+    })
+
+
+}
